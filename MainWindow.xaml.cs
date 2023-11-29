2363cb0319b286e76e18d477c133b86a5e40d16a
iTrooz <hey@itrooz.fr>
2023-11-29 22:05:16 +0100

    rename project to MyProject + install app to lib/Net45

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace squirrel
+namespace Myproject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
