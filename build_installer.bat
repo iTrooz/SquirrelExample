12cb6301dc4c67c8f9be9412e4decf18690a4c06
iTrooz <hey@itrooz.fr>
2023-11-29 22:29:33 +0100

    Install squirrel in installer build script

--- build_installer.bat
+++ build_installer.bat
@@ -1,5 +1,9 @@
+
+REM Install Squirrel locally so that we can access squirrel.exe
+nuget install Squirrel.Windows -o local/squirrel
+
 REM Create .nupkg file from .nuspec
 nuget pack MyProject.nuspec -Properties Configuration=Release || exit /b
 
 REM Package it into a setup.msi and setup.exe (the latter doesn't require admin privileges to install), outputted to Releases/
-local\Squirrel.windows.2.0.1\tools\Squirrel.exe --releasify MyProject.1.0.0.nupkg || exit /b
+local\squirrel\Squirrel.windows.2.0.1\tools\Squirrel.exe --releasify MyProject.1.0.0.nupkg || exit /b
