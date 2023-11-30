772eaf4ec0cb5668957d9246b96cf1faebb5727b
iTrooz <hey@itrooz.fr>
2023-11-30 13:33:14 +0100

    use publish instead of build

--- build_installer.bat
+++ build_installer.bat
@@ -6,7 +6,7 @@ REM Clean
 DEL /F /Q build Releases
 
 REM Build project
-dotnet build -o build
+dotnet publish -o build
 
 REM Create .nupkg file from .nuspec
 nuget pack MyProject.nuspec -Properties Configuration=Release || exit /b
